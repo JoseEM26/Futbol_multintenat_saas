5cdd900a9a4315516acd4763f991725cc1177098
JoseEM26 <joseangelespinozamorales@gmail.com>
2026-06-07 00:11:26 -0500

    refactor: remove unused NestJS backend, consolidate into Next.js

--- apps/backend/src/modules/canchas/canchas.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { CanchasService } from './canchas.service';
-import { CanchasController } from './canchas.controller';
-
-@Module({
-  controllers: [CanchasController],
-  providers: [CanchasService],
-})
-export class CanchasModule {}
